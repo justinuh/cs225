67d88b766230188cd9d5814059105d90b45569c9
Justin Kuo <j.kuo7854@edmail.edcc.edu>
2016-04-27 23:31:58 -0700

    This is revised version of lab 6 and listen to grant on how to approach it.

--- lab6.sh
+++ lab6.sh
@@ -1,50 +1,34 @@
-#! /bin/bash 
+#! /bin/bash -v
 
 rm -f /root/Projects/cs225/medialab/mpglist.txt
-MEDIAXML="$(grep filename /root/Projects/cs225/medialab/media.xml| cut -d'>' -f2| cut -d'<' -f1 )"
-MEDIALAB="$(ls -A /root/Projects/cs225/medialab/.) "
+MEDIAXML=$(grep "filename"  /root/Projects/cs225/medialab/media.xml| cut -d'>' -f2| cut -d'<' -f1 | sort | uniq | grep "NULL" )
+MEDIALAB=$(ls -A /root/Projects/cs225/medialab) 
+RAIN=$(cat /root/Projects/cs225/medialab/media.xml)
 
+echo "files not found in media.xml:"
 
-NUM1=0
-NUM2=0
-
-for filename in $MEDIAXML ; do
-
-	if [[ ! -e /root/Projects/cs225/medialab/"$filename" ]] ;then 
-		echo "$filename"
-		echo "$filename" >> /root/Projects/cs225/medialab/mpglist.txt
-		(( NUM1++ ))
-	fi
-done
-if [[ $NUM1 -eq  0 ]]
-then	
-	echo "No files are not in media.xml"
-	echo "No files are not in media.xml" >> /root/Projects/cs225/medialab/mpglist.txt
-fi
-echo
-echo >> /root/Projects/cs225/medialab/mpglist.txt
-echo "Files not in medialab directory:"
-echo "Files not in medialab directory:" >> /root/Projects/cs225/medialab/mpglist.txt
-
-for items in $MEDIALAB
-do
-	if [[ -n "$(grep -v $items /root/Projects/cs225/medialab/media.xml)" ]]
+for filename in $MEDIALAB
+do 
+	if ! grep $filename /root/Projects/cs225/medialab/media.xml
 	then
-		echo "$items"
-		echo "$items" >> /root/Projects/cs225/medialab/mpglist.txt
-		(( NUM2++ ))
+		echo "$filename"
+		(( FileCounter++))
 	fi
 done
 
-if [[ $NUM2 -eq  0 ]]
-then 
-	echo "No files are not in medialab directory"
-	echo "No files are not in medialab directory" >> /root/Projects/cs225/medialab/mpglist.txt 
-fi 
+echo "files not in medialab:"
 
-echo 
-echo >> /root/Projects/cs225/medialab/checklist.txt
-echo "$NUM1 media files in medialab directory that are NOT list in media.xml"
-echo "$NUM1 media files in medialab directory that are NOT listed in media.xml" >> /root/Projects/cs225/medialab/mpglist.txt
-echo "$NUM2 media files in media.xml that are Not in medialab directory"
-echo "$NUM2 media files in media.xml that are NOT in medialab directory" >> /root/Projects/cs225/medialab/mpglist.txt  
+for item in $MEDIAXML
+do 
+ 	if [[ -e /root/Projects/cs225/medialab/$item ]]
+	then	
+		echo "$item"
+		(( LINECounter++ ))
+	fi
+done
+
+
+
+echo "$FileCounter files in medialab that are not in media.xml"
+echo "$LineCounter files in media.xml that are not in medialab"
+
